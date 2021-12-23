32b1e79560b81bc64545bb6bd4fd0643302b176c
Dilshod1000 <dilshodazim0@gmail.com>
2021-12-23 03:54:34 +0300

    Создано решение 1-3

---
+++ 1-3/1-3/1-3.cpp
@@ -0,0 +1,35 @@
+﻿#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+
+
+using namespace std;
+
+/*
+* @brief вычисление пути пройдёным машиной
+* @param a совершенная работа
+* @param f сила тяги
+* @return путь пройденный автомобилем
+*/
+double getMass(const double v, const double m);
+
+int main()
+{
+	setlocale(0, "rus"); //перевод на русские буквы
+	cout << "Введите массу пули - M: ";
+	double m = 0;
+	cin >> m;
+	cout << "Ведите скорость - V: ";
+	double v = 0;
+	cin >> v;
+
+	//double power = m * (v * v) / 2;
+
+	cout << "Дж: " << getMass(v, m) << endl;
+	return 0;
+}
+
+double getMass(const double v, const double m)
+{
+	return  (m * (v * v) / 2);
+}
